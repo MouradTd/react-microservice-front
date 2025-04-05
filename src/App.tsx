506f88f8948f8219a1754c214f1000e1070af0f6
Mourad <mourad.arejdal@gmail.com>
2025-04-05 10:21:10 +0000

    fix app layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,19 @@
-import {Footer,Header, Sidebar} from '@/core';
+import React from 'react';
+import { Footer, Header, Sidebar } from '@/core';
 import { Route } from './routes/routes';
 
 const App: React.FC = () => {
     // return <Route />;
     return (
-        <div className="flex min-h-screen">
-            <Sidebar />
-            <div className="flex-grow ml-64 p-4">
-                <Header />
-                <main>
+        <div className="flex flex-col min-h-screen">
+            <Header />
+            <div className="flex flex-grow">
+                <Sidebar />
+                <main className="flex-grow ml-64 p-4">
                     <Route />
                 </main>
-                <div className=''>
-                    <Footer />
-                </div>
             </div>
+            <Footer />
         </div>
     );
 };
